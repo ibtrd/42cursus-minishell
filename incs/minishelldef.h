c79cc69c7ce5c8da840c17923e11e452572e1ed0
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-02-15 17:46:51 +0100

    Add builtins defines

---
+++ incs/minishelldef.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishelldef.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 16:01:05 by ibertran          #+#    #+#             */
+/*   Updated: 2024/02/15 17:46:16 by ibertran         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELLDEF_H
+# define MINISHELLDEF_H
+
+# define _ECHO "echo"
+# define _CD "cd"
+# define _PWD "pwd"
+# define _EXPORT "export"
+# define _UNSET "unset"
+# define _ENV "env"
+# define _EXIT "exit"
+
+#endif
