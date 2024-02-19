25dd61ccbeadd4e63dbd074ecaffb56fcd831db4
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-02-19 01:24:04 +0100

    Add documentation for dup_cmdline()

---
+++ incs/minishell_parsing.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_parsing.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 00:06:16 by ibertran          #+#    #+#             */
+/*   Updated: 2024/02/19 00:07:08 by ibertran         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_PARSING_H
+# define MINISHELL_PARSING_H
+
+int	dup_cmdline(char *cmdline, char **dup);
+
+#endif
--- srcs/dup_cmdline.c
+++ srcs/dup_cmdline.c
@@ -6,22 +6,34 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 21:24:34 by ibertran          #+#    #+#             */
-/*   Updated: 2024/02/18 21:44:43 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/02/19 01:23:04 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-#include "minishelldef.h"
-#include "ft_vector.h"
-#include "ft_char.h"
-#include "ft_string.h"
+#include "minishell_def.h"
+#include "libft.h"
 
-int	dup_cmdline(char *cmdline, char **dup)
+/*
+	DESCRIPTION
+	The dup_cmdline() add a space character before and after
+	each operator, if they are are not already prefixed or suffixed
+	by a whitespace character. The resulting string is attached to
+	the string pointed by ptr. Memory for the new string is obtained
+	with malloc(), and can be freed with free().
+	
+	RETURN VALUE
+	The dup_cmdline() function return 0.
+	On error, dup_cmdline() return -1 and the string pointed by ptr
+	is set to NULL.
+*/
+
+int	dup_cmdline(char *cmdline, char **ptr)
 {
-	t_vector		vector;
-	int				status;
-	int				i;
+	t_vector	vector;
+	int			status;
+	int			i;
 
 	status = ft_vector_init(&vector, sizeof(char));
 	i = 0;
@@ -37,8 +49,8 @@ int	dup_cmdline(char *cmdline, char **dup)
 		i++;
 	}
 	status = ft_vector_add(&vector, "\0");
-	*dup = ft_strdup(vector.ptr);
-	if (*dup)
+	*ptr = ft_strdup(vector.ptr);
+	if (*ptr)
 		status = FAILURE;
 	ft_vector_free(&vector);
 	return (status);
