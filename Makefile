328950cfd66b38267274105d94f681162258d86e
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-03 02:11:03 +0100

    Rewrok lexer tet program

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/14 22:03:24 by ibertran          #+#    #+#              #
-#    Updated: 2024/03/03 01:50:24 by ibertran         ###   ########lyon.fr    #
+#    Updated: 2024/03/03 02:09:35 by ibertran         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -197,7 +197,7 @@ print% :
 run :	$(NAME)
 	./$(NAME)
 
-VALGRIND = valgrind --suppressions=.valgrindignore.txt --leak-check=full
+VALGRIND = valgrind -q --suppressions=.valgrindignore.txt --leak-check=full
 
 .PHONY : valgrind
 valgrind : debug
--- srcs/tests/main_lexer.c
+++ srcs/tests/main_lexer.c
@@ -6,14 +6,17 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 00:30:10 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/03 01:14:08 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/03 02:10:10 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "minishelldef.h"
 #include "ast.h"
@@ -25,12 +28,15 @@ int	main(void)
 	t_vector			lexer;
 	char				*input;
 	char 				*dup = NULL;
+	__pid_t				pid;
+	int					status;
 
 	do
 	{
 		input = readline("\nEnter a command line: ");
 		if (!input)
 			return (0);
+		add_history(input);
 		if (syntax_checker(input))
 		{
 			free(input);
@@ -40,12 +46,23 @@ int	main(void)
 		free(input);
 		lexer_build(dup, &lexer);
 
-		if (!lexer_analysis(&lexer, 0))
-			printf("\e[32;1mValid!\e[0m\n\n");
-		else
-			printf("\e[31;1mInvalid!\e[0m\n\n");
-		
-		printf("\n");
+		pid = fork();
+		if (!pid)
+		{
+			close(1);
+			status = lexer_analysis(&lexer, 0);
+			ft_vector_free(&lexer, NULL);
+			free(dup);
+			exit(status);
+		}
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			if (WEXITSTATUS(status) == 0)
+				printf("\e[32;1mValid!\e[0m\n");
+			else
+				printf("\e[31;1mInvalid!\e[0m\n");
+		}
 		ft_vector_free(&lexer, NULL);
 		free(dup);
 	} while (1);
