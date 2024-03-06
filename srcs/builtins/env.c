ee422e639af5450f85f2934d8caf270d82585d26
Kalde Chillon <kchillon@z1r5p5.42lyon.fr>
2024-03-06 20:07:34 +0100

    Add env builtin

---
+++ srcs/builtins/env.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 19:30:06 by kchillon          #+#    #+#             */
+/*   Updated: 2024/03/06 19:34:55 by kchillon         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+#include <stdio.h>
+
+int	builtin_env(t_vector *env)
+{
+	size_t		i;
+	t_env_var	*env_var;
+
+	if (!env)
+		return (1);
+	env_var = (t_env_var *)env->ptr;
+	if (!env_var)
+		return (1);
+	i = 0;
+	while (i < env->total)
+	{
+		printf("%s=%s\n", env_var[i].name, env_var[i].value);
+		i++;
+	}
+	return (0);
+}
---
+++ srcs/env/free_var.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_var.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 19:27:27 by kchillon          #+#    #+#             */
+/*   Updated: 2024/03/06 19:28:44 by kchillon         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+#include <stdlib.h>
+
+void	free_var(void *var)
+{
+	t_env_var	*env_var;
+
+	env_var = (t_env_var *)var;
+	free(env_var->name);
+	free(env_var->value);
+}
