8964401c2d9ced66e0ace5a3877dbe1919f901b8
Kalde Chillon <kchillon@z1r5p5.42lyon.fr>
2024-03-08 15:33:10 +0100

    Update protected init_env

--- srcs/env/init_env.c
+++ srcs/env/init_env.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:22:58 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/08 14:12:51 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/08 15:32:21 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,62 @@ int	init_env(t_vector *envv, char **env)
 	size_t		i;
 	char		*tmp;
 	t_env_var	env_var;
+	int			error;
 
-	if (ft_vector_init(envv, sizeof(t_env_var), 0))
-		return (1);
+	error = ft_vector_init(envv, sizeof(t_env_var), 0);
+	error = 0;
 	i = 0;
-	while (env[i])
+	while (!error && env[i])
 	{
 		tmp = ft_strtok(env[i], "=");
 		env_var.name = ft_strdup(tmp);
+		if (!env_var.name)
+			error = 1;
 		tmp = ft_strtok(NULL, "");
 		env_var.value = ft_strdup(tmp);
-		ft_vector_add(envv, &env_var);
+		if (!env_var.value)
+			error = 1;
+		if (!error)
+			error = ft_vector_add(envv, &env_var);
 		i++;
 	}
-	return (0);
+	if (error)
+		ft_vector_free(envv, &free_var);
+	return (error);
 }
+
+// int	init_env(t_vector *envv, char **env)
+// {
+// 	size_t		i;
+// 	char		*tmp;
+// 	t_env_var	env_var;
+// 	int			error;
+
+// 	if (ft_vector_init(envv, sizeof(t_env_var), 0))
+// 		return (1);
+// 	error = 0;
+// 	i = 0;
+// 	while (env[i])
+// 	{
+// 		tmp = ft_strtok(env[i], "=");
+// 		env_var.name = ft_strdup(tmp);
+// 		if (!env_var.name)
+// 			error = 1;
+// 		tmp = ft_strtok(NULL, "");
+// 		env_var.value = ft_strdup(tmp);
+// 		if (!env_var.value)
+// 			error = 1;
+// 		if (error)
+// 		{
+// 			ft_vector_free(envv, &free_var);
+// 			return (1);
+// 		}
+// 		if (ft_vector_add(envv, &env_var))
+// 		{
+// 			ft_vector_free(envv, &free_var);
+// 			return (1);
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
--- srcs/tests/main_init_env.c
+++ srcs/tests/main_init_env.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:41:08 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/08 14:31:03 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/08 15:22:40 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int	main(int ac, char **av, char **env)
 	envv = (t_vector){0};
 	init_env(&envv, env);
 	builtin_env(&envv);
+	printf("\n");
 	printf("ft_getenv(envv, \"PATH\") = %s\n", ft_getenv(&envv, "PATH"));
 	ft_vector_free(&envv, &free_var);
 	return (0);
