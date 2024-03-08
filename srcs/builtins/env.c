9858697baf929d40cd1b2107e903a2ce74743ccb
Kalde Chillon <kchillon@z1r5p5.42lyon.fr>
2024-03-08 15:35:19 +0100

    Update protected builtin_env

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 19:30:06 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/08 13:44:18 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/08 15:34:47 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int	builtin_env(t_vector *env)
 	size_t		i;
 	t_env_var	*env_var;
 
-	if (!env)
-		return (1);
-	env_var = (t_env_var *)env->ptr;
+	env_var = ft_vector_get(env, 0);
 	if (!env_var)
 		return (1);
 	i = 0;
