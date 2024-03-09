f3ca72b41f6d76bb362a1615a4269e7338fdac9f
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-09 16:17:21 +0100

    Add missing include to expander.h

--- incs/expander.h
+++ incs/expander.h
@@ -6,12 +6,14 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 00:12:59 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/09 03:18:43 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/09 16:15:21 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPANDER_H
 # define EXPANDER_H
+
+#include "ft_vector.h"
 
 int	tilde_expansion(char **ptr);
 int	envars_expansion(char **ptr, t_vector *env);
