5c14720e4848fc7bdd3243d634280122e784664e
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-10 16:53:17 +0100

    Fix vector_alloc uninitialized value

--- libft/srcs/vector/ft_vector_allocate.c
+++ libft/srcs/vector/ft_vector_allocate.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 20:49:50 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/10 16:48:49 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/10 16:52:08 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	ft_vector_alloc(t_vector **ptr, t_vinfos infos, size_t n)
 	new = malloc(sizeof(t_vector) * n);
 	if (!new)
 		return (FAILURE);
+	i = 0;
 	while (i < n)
 	{
 		if (ft_vector_init(new + i, infos))
