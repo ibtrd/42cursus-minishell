9c00579d87e632b36435664273637497918d9a13
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-11 00:00:54 +0100

    Norm

--- srcs/ast/ast_print.c
+++ srcs/ast/ast_print.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 19:37:35 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/10 23:38:29 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/10 23:57:17 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,42 @@
 
 #include "ast.h"
 
-#define COUNT 3
+void	printnode(int fd, t_astnode *node);
 
-void printnode(int fd, t_astnode *node);
-
-void dprint_astUtil(int fd, t_astnode* root, int space)
+void	dprint_ast_util(int fd, t_astnode *root, int space)
 {
-    if (root == NULL)
-        return;
-    space += COUNT;
- 
-    dprint_astUtil(fd, root->right, space);
- 
-    for (int i = COUNT; i < space; i++)
-        dprintf(fd, " ");
-    printnode(fd, root);
- 
-    dprint_astUtil(fd, root->left, space);
+	int	i;
+
+	if (root == NULL)
+		return ;
+	space += 3;
+	dprint_ast_util(fd, root->right, space);
+	i = 3;
+	while (i++ < space)
+		dprintf(fd, " ");
+	printnode(fd, root);
+	dprint_ast_util(fd, root->left, space);
 }
- 
-void dprint_ast(int fd, t_astnode* root, char *color)
+
+void	dprint_ast(int fd, t_astnode *root, char *color)
 {
 	if (color)
 		dprintf(2, "\n%s", color);
 	else
 		dprintf(2, "\n");
-    dprint_astUtil(fd, root, 0);
-	dprintf(2,"\e[0m\n---------------------------------------------\n");
+	dprint_ast_util(fd, root, 0);
+	dprintf(2, "\e[0m\n---------------------------------------------\n");
 }
 
-void printnode(int fd, t_astnode *node)
+void	printnode(int fd, t_astnode *node)
 {
-	size_t 		i = 0;
+	const char	*operator[] = {"&&", "||", "|", "<", ">", "<<", ">>"};
 	t_vector	*str;
+	size_t		i;
 
 	i = 0;
-	if (node->type == _AND)
-		dprintf(fd, "&&\n");
-	if (node->type == _OR)
-		dprintf(fd, "||\n");
-	if (node->type == _PIPE)
-		dprintf(fd, "|\n");
-	if (node->type == _INPUT)
-	{
-		str = ft_vector_get(node->args, i);
-		dprintf(fd, "< ");
-	}
-	if (node->type == _OUTPUT)
-	{
-		str = ft_vector_get(node->args, i);
-		dprintf(fd, "> ");
-	}
-	if (node->type == _HEREDOC)
-	{
-		str = ft_vector_get(node->args, i);
-		dprintf(fd, "<< ");
-	}
-	if (node->type == _APPEND)
-	{
-		str = ft_vector_get(node->args, i);
-		dprintf(fd, ">> ");
-	}
+	if (node->type >= _AND && node->type <= _APPEND)
+		dprintf(fd, "%s ", operator[node->type]);
 	if (node->type == _CMD || (node->type >= _INPUT && node->type <= _APPEND))
 	{
 		str = ft_vector_get(node->args, i);
@@ -83,6 +58,6 @@ void printnode(int fd, t_astnode *node)
 			dprintf(fd, "%s ", (char *)str->ptr);
 			str = ft_vector_get(node->args, i);
 		}
-		dprintf(fd, "\n");
 	}
+	dprintf(fd, "\n");
 }
