67024f49a0a2688fea7b59363b4ac69557673bdc
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-13 04:13:31 +0100

    Fix vector_search + add corresponding test

--- libft/srcs/vector/ft_vector_search.c
+++ libft/srcs/vector/ft_vector_search.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 03:38:16 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/13 03:51:51 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/13 04:12:41 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_vector_search(t_vector *v, const void *item)
 	while (index < v->total)
 	{
 		ptr = v->ptr + index * v->size;
-		if (ft_memcmp(ptr, item, v->size))
+		if (!ft_memcmp(ptr, item, v->size))
 			return (ptr);
 		index++;
 	}
