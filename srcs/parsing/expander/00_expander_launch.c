dee9101678b33bb287e3d4efd7e072187d447e14
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-03-15 05:01:38 +0100

    Add quote removal to expander

--- srcs/parsing/expander/00_expander_launch.c
+++ srcs/parsing/expander/00_expander_launch.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 19:20:50 by ibertran          #+#    #+#             */
-/*   Updated: 2024/03/15 03:16:48 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/03/15 04:57:11 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ static int	expand_node(t_astnode *node, t_vector *env)
 	t_vector	*arg;
 	size_t		i;
 
+	if (node->type == _HEREDOC)
+		return (SUCCESS);
 	if (node->type != _CMD && (node->type < _INPUT || node->type > _APPEND))
 		return (SUCCESS);
 	i = 0;
@@ -75,13 +77,16 @@ static int	expand_string(t_vector *args, size_t *index, t_vector *env)
 		return (FAILURE);
 
 	str = ft_vector_get(args, *index);
-	printf("\nWORD SPLITTING\n"); //DEBUG
+	printf("\nWORD SPLIT\n"); //DEBUG
 	ft_vector_print(ft_vector_get(args, *index), ft_vprint_char, "arg "); //DEBUG
 	ft_vector_print(ft_vector_get(args, *index), debug_print_mask, "mask"); //DEBUG
 
-	// if (quote_removal(str, masks, index))
-	// 	return (FAILURE);
+	if (quote_removal(str))
+		return (FAILURE);
 
-	// printf("\n"); //DEBUG
+	printf("\nQUOTE_REMOVAL\n"); //DEBUG
+	ft_vector_print(ft_vector_get(args, *index), ft_vprint_char, "arg "); //DEBUG
+	ft_vector_print(ft_vector_get(args, *index), debug_print_mask, "mask"); //DEBUG
+
 	return (SUCCESS);
 }
