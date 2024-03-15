8b3eca4c1e821215c2c08e45d0074c06e0c1e476
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-03-15 18:36:19 +0100

    Update complete cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:33:54 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/15 17:57:01 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/15 18:34:24 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,59 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+static int	resolve_dir(char **dir, t_vector *env, char **argv)
+{
+	size_t	argc;
+
+	if (argv && *argv)
+	{
+		argc = 0;
+		while (argv[argc])
+			argc++;
+		if (argc > 1)
+		{
+			ft_dprintf(2, "%s: cd: too many arguments\n", __MINISHELL);
+			return (1);
+		}
+		*dir = argv[0];
+		if (!ft_strcmp(*dir, "-"))
+			*dir = ft_getenv(env, "OLDPWD");
+	}
+	if (!*argv)
+		*dir = ft_getenv(env, "HOME");
+	return (0);
+}
+
 static int	update_wd(t_vector *env)
 {
-	char	*tmp;
+	char	*pwd;
+	char	*oldpwd;
 	char	*cwd;
+	int		error;
 
-	tmp = ft_strjoin("OLDPWD=", ft_getenv(env, "PWD"));
-	if (!tmp || update_var(env, tmp))
-		return (1);
-	free(tmp);
 	cwd = getcwd(NULL, 0);
-	tmp = ft_strjoin("PWD=", cwd);
+	pwd = ft_strjoin("PWD=", cwd);
 	free(cwd);
-	if (!tmp || update_var(env, tmp))
-		return (1);
-	free(tmp);
-	return (0);
+	oldpwd = ft_strjoin("OLDPWD=", ft_getenv(env, "PWD"));
+	error = !oldpwd || !pwd || !cwd;
+	if (!error)
+		error = update_var(env, oldpwd) || update_var(env, pwd);
+	free(oldpwd);
+	free(pwd);
+	return (error);
 }
 
 int	builtin_cd(t_executor *exec, char **argv)
 {
-	size_t	argc;
+	char	*dir;
 
 	(void)exec;
 	if (!argv)
 		return (1);
-	argc = 0;
-	while (argv[argc])
-		argc++;
-	if (argc > 1)
-	{
-		ft_dprintf(2, "%s: cd: too many arguments\n", __MINISHELL);
+	dir = NULL;
+	if (resolve_dir(&dir, exec->env, argv))
 		return (1);
-	}
-	if (chdir(argv[0]) == -1)
+	if (chdir(dir) == -1)
 	{
 		ft_dprintf(2, "%s: cd: %s: %s\n", __MINISHELL, argv[0], strerror(errno));
 		return (1);
