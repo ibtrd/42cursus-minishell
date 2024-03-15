c3fb30885eb323234f23d42d24efb619824d4e18
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-03-15 16:41:37 +0100

    Add pwd

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,15 +6,30 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:34:06 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/14 16:44:37 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/15 16:40:09 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
+#include "env.h"
+#include "minishelldef.h"
 
-int	builtin_pwd(t_executor *exec, char **argv)
+#include <unistd.h>
+#include <stdlib.h>
+
+int    builtin_pwd(t_executor *exec, char **argv)
 {
-	(void)argv;
-	(void)exec;
-	return (0);
+    char    *pwd;
+
+    (void)argv;
+    (void)exec;
+    pwd = getcwd(NULL, 0);
+    if (!pwd)
+    {
+        ft_dprintf(2, "%s: Cannot get current working directory path\n", __MINISHELL);
+        return (1);
+    }
+    printf("%s\n", pwd);
+    free(pwd);
+    return (0);
 }
