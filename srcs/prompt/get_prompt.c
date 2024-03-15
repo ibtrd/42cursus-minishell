780a84affc8a4684a6096ee59780a1afcf8bba12
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-03-15 21:21:51 +0100

    Fix prompt (doesn't use env var PWD)

--- srcs/prompt/get_prompt.c
+++ srcs/prompt/get_prompt.c
@@ -6,25 +6,29 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 19:14:29 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/15 19:36:19 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/15 21:15:14 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "env.h"
+#include "minishelldef.h"
+
+#include <stdlib.h>
+#include <unistd.h>
 
 char	*get_prompt(t_vector *env)
 {
 	char	*prompt;
 	char	*tmp;
 
-	tmp = ft_getenv(env, "PWD");
+	(void)env;
+	tmp = getcwd(NULL, 0);
 	if (!tmp)
-		tmp = ft_getenv(env, "HOME");
-	if (!tmp)
-		tmp = ft_strdup("minishell");
+		tmp = ft_strdup(__MINISHELL);
 	if (!tmp)
 		return (NULL);
 	prompt = ft_strjoin2("\e[34m", tmp, "$\e[0m ");
+	free(tmp);
 	return (prompt);
 }
