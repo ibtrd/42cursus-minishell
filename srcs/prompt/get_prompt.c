195680ea2fe7d91329eccc28294f6938e39464a2
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-03-16 17:41:37 +0100

    Update only show cwd

--- srcs/prompt/get_prompt.c
+++ srcs/prompt/get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 19:14:29 by kchillon          #+#    #+#             */
-/*   Updated: 2024/03/15 21:15:14 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/03/16 17:39:43 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+static int    user_dir(t_vector *env, char **cwd)
+{
+    char    *home;
+    size_t    len;
+
+    home = ft_getenv(env, "HOME");
+    if (!home)
+	{
+        return (0);
+	}
+    len = ft_strlen(home);
+    if (!ft_strncmp(*cwd, home, len) && !(*cwd)[len])
+    {
+        free(*cwd);
+        *cwd = ft_strdup("~");
+        if (!*cwd)
+            return (1);
+    }
+    // tmp = ft_strstr(*cwd, home);
+    // if (tmp)
+    // {
+    //     *cwd = ft_strjoin2("~", tmp + ft_strlen(home));
+    //     if (!*cwd)
+    //         return (0);
+    // }
+    return (0);
+}
+
 char	*get_prompt(t_vector *env)
 {
 	char	*prompt;
+	char	*cwd;
 	char	*tmp;
 
-	(void)env;
-	tmp = getcwd(NULL, 0);
-	if (!tmp)
-		tmp = ft_strdup(__MINISHELL);
-	if (!tmp)
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		cwd = ft_strdup(__MINISHELL);
+	if (!cwd)
 		return (NULL);
-	prompt = ft_strjoin2("\e[34m", tmp, "$\e[0m ");
-	free(tmp);
+	if (user_dir(env, &cwd))
+		return (NULL);
+	tmp = ft_strrchr(cwd, '/');
+	if (!tmp)
+		prompt = cwd;
+	else if (*(tmp + 1) && *(tmp + 1) != '/')
+		prompt = ft_strrchr(tmp, '/') + 1;
+	else
+		prompt = tmp;
+	prompt = ft_strjoin2("\e[34m", prompt, "\e[0m ");
+	free(cwd);
 	return (prompt);
 }
