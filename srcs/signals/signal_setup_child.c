4720174873cddbb0c0a6af25fca0e602272950b5
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-04-14 14:23:51 +0200

    Norm

--- srcs/signals/signal_setup_child.c
+++ srcs/signals/signal_setup_child.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_setup_child.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:54:25 by kchillon          #+#    #+#             */
-/*   Updated: 2024/04/14 13:50:29 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/04/14 14:23:38 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ void	signal_setup_child(void)
 	act = (struct sigaction){0};
 	act.sa_flags = SA_RESTART;
 	act.sa_handler = SIG_DFL;
-	// act.sa_handler = &child_int_handler;
 	sigaction(SIGINT, &act, NULL);
-	// act.sa_handler = &child_quit_handler;
 	sigaction(SIGQUIT, &act, NULL);
 	sigaction(SIGTERM, &act, NULL);
 }
