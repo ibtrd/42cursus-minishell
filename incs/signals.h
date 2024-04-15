92806870314f359d044ba8ecafb55b3aca2f4f47
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-04-15 17:42:21 +0200

    Fix too many new line

--- incs/signals.h
+++ incs/signals.h
@@ -6,14 +6,14 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:02:34 by kchillon          #+#    #+#             */
-/*   Updated: 2024/04/14 19:29:45 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/04/15 17:39:50 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
-# define __QUIT "Quit (core dumped)"
+# define __QUIT "Quit (core dumped)\n"
 
 int		ft_rl_event(void);
 void	exec_handler(int sig);
