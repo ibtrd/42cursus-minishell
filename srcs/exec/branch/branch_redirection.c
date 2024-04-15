cd3f0784276f2b0c6ca27e8aaa1b27b7ddaace9a
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-04-15 17:12:59 +0200

    Fix ambiguous redirect

--- srcs/exec/branch/branch_redirection.c
+++ srcs/exec/branch/branch_redirection.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:01:09 by kchillon          #+#    #+#             */
-/*   Updated: 2024/04/15 16:45:22 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/04/15 17:12:11 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ int	branch_redirection(t_executor *exec)
 	int			ret;
 	char		*no_expand;
 
+	ret = 0;
 	no_expand = NULL;
 	if (exec->node->type != _HEREDOC)
 	{
@@ -46,11 +47,10 @@ int	branch_redirection(t_executor *exec)
 		if (!no_expand)
 			return (1);
 		ret = expand_node(exec->node, exec->minishell);
-		free(no_expand); // BUG: no_expand is freed here, but it is used later in open_redirect
-		if (ret)
-			return (1);
 	}
-	ret = open_redirect(exec, no_expand);
+	if (!ret)
+		ret = open_redirect(exec, no_expand);
+	free(no_expand); // BUG: no_expand is freed here, but it is used later in open_redirect
 	if (ret)
 		return (1);
 	node = exec->node;
