7a338ae415910db3f4d4c75a52db74b18ba8758b
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-04-15 17:16:31 +0200

    Remove comments

--- srcs/exec/retrieve_status.c
+++ srcs/exec/retrieve_status.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:32:47 by kchillon          #+#    #+#             */
-/*   Updated: 2024/04/15 16:10:03 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/04/15 17:16:08 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	retrieve_status(pid_t pid)
 {
 	int	status;
 
-	pid = waitpid(pid, &status, 0); // PROTECT
+	pid = waitpid(pid, &status, 0);
 	if (pid == -1 && errno != ECHILD)
 		return (1);
 	if (WIFEXITED(status))
